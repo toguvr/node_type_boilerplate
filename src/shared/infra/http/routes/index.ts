a9e7171320aae63a7d248f129e156580da1bfce5
Augusto Telles <40339166+toguvr@users.noreply.github.com>
2020-05-23 15:47:37 -0300

    push with tests and new patterns

---
+++ src/shared/infra/http/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import appointmentRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import userRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentRouter);
+routes.use('/users', userRouter);
+routes.use('/sessions', sessionsRouter);
+
+export default routes;
